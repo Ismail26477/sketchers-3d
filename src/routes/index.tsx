18cabf2bdb2c1096807f523385fbde960d2a88a4
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-07-28 02:13:42 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,9 +51,7 @@ import g38 from "@/assets/38.jpeg";
 import g39 from "@/assets/39.jpeg";
 import g40 from "@/assets/40.jpeg";
 import g41 from "@/assets/41.png";
-import g42 from "@/assets/42.png";
 import g43 from "@/assets/43.png";
-import g44 from "@/assets/44.png";
 import g45 from "@/assets/45.png";
 import g46 from "@/assets/46.jpeg";
 import g47 from "@/assets/47.jpeg";
@@ -270,7 +268,7 @@ const GALLERY = [
   g11, g12, g13, g14, g15, g16, g17, g18, g19, g20,
   g21, g22, g23, g24, g25, g26, g27, g28, g29, g30,
   g31, g32, g33, g34, g35, g36, g37, g38, g39, g40,
-  g41, g42, g43, g44, g45, g46, g47, g48, g49, g50,
+  g41, g43, g45, g46, g47, g48, g49, g50,
   g51, g52, g53, g54, g55, g56, g57, g58, g59, g60,
   g61, g62, g63, g64, g65, g66, g67, g68, g69, g70,
   g71, g72, g73, g74, g75, g76, g77, g78, g79, g80,
