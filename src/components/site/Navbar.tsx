51072e41baef65ab696725ea119e5bf5d895a97b
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-07-29 12:15:22 +0530

    Update Navbar.tsx

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -18,9 +18,7 @@ export function Navbar() {
   return (
     <header
       className={`fixed inset-x-0 top-0 z-50 transition-all duration-500 ease-in-out ${
-        scrolled
-          ? "bg-white/0 border-white/5"
-          : "bg-transparent"
+        scrolled ? "bg-white/0 border-white/5" : "bg-transparent"
       }`}
     >
       <nav className="mx-auto flex h-24 max-w-[1440px] items-center justify-between px-4 sm:h-24 sm:px-6 lg:h-24 lg:px-12">
